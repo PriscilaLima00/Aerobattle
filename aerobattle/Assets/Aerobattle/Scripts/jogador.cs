88839bc688eda25b6e3b6f43cda938401ec07ecd
PRISCILA RAFAELA LIMA DANTAS <d.priscila@academico.ifrn.edu.br>
2024-07-24 16:15:10 -0300

    Movimentação e tiro

---
+++ aerobattle/Assets/Aerobattle/Scripts/jogador.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jogador : MonoBehaviour
+{
+    public Rigidbody2D rigi;
+
+    public float velocidadeDaNave;
+    public bool temLaserDuplo;
+
+    private Vector2 teclasApertadas;
+
+    public GameObject laser;
+
+    public Transform localDoDisparoUnico;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        temLaserDuplo = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentoPlay();
+        AtirarLaser();
+    }
+    private void MovimentoPlay()
+    {
+        teclasApertadas = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        rigi.velocity = teclasApertadas.normalized * velocidadeDaNave;
+    }
+
+    private void AtirarLaser()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (temLaserDuplo == false)
+            {
+                Instantiate(laser,localDoDisparoUnico.position,localDoDisparoUnico.rotation);
+            } 
+        }
+    }
+}
