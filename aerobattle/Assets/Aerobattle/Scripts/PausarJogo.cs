ddd60c894ff906d3eb994d849a091a02dbc0982c
PRISCILA RAFAELA LIMA DANTAS <d.priscila@academico.ifrn.edu.br>
2024-08-06 14:23:36 -0300

    pause funcionando

---
+++ aerobattle/Assets/Aerobattle/Scripts/PausarJogo.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausarJogo : MonoBehaviour
+{
+    public bool jogoPausado;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        jogoPausado = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pausar();
+        }
+    }
+    
+    private void Pausar()
+    {
+        if (jogoPausado == false)
+        {
+            Time.timeScale = 0f;
+            jogoPausado = true;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            jogoPausado = false;
+        }
+    }
+}
