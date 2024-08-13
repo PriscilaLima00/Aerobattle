fa8a9805568c654018bc33372df005e8e342827f
PRISCILA RAFAELA LIMA DANTAS <d.priscila@academico.ifrn.edu.br>
2024-08-13 11:48:29 -0300

    programando o inimigo

---
+++ aerobattle/Assets/Aerobattle/Scripts/Inimigo.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    public GameObject laserDoInimigo;
+    public Transform localDoDisparo;
+    
+    public float velocidadeDoInimigo;
+
+    public float tempoMaximoEntreOsLasers;
+    public float tempoAtualDosLasers;
+
+    public bool inimigoAtirador;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentarInimigo();
+        if (inimigoAtirador == true)
+        {
+            AtirarLaser();
+        }
+        
+    }
+
+    private void MovimentarInimigo()
+    {
+        transform.Translate(Vector3.down * velocidadeDoInimigo *Time.deltaTime);
+    }
+
+    private void AtirarLaser()
+    {
+        tempoAtualDosLasers -= Time.deltaTime;
+
+        if (tempoAtualDosLasers <= 0)
+        {
+            Instantiate(laserDoInimigo, localDoDisparo.position, Quaternion.Euler(0f, 0f, 90f));
+            tempoAtualDosLasers = tempoAtualDosLasers;
+        }
+    }
+}
