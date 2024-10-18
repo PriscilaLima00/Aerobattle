c89f54aac25d29092cbd6aa66e94e66024715da0
PRISCILA RAFAELA LIMA DANTAS <d.priscila@academico.ifrn.edu.br>
2024-10-18 16:18:24 -0300

    ...

--- aerobattle/Assets/Aerobattle/Scripts/NuvemDePoeira.cs
+++ aerobattle/Assets/Aerobattle/Scripts/NuvemDePoeira.cs
@@ -7,11 +7,14 @@ public class NuvemDePoeira : MonoBehaviour
     public int danoParaCausar = 1; 
     public float tempoDeDano = 1f; 
     private float tempoDesdeUltimoDano = 0.1f;
+    public int velocidade;
 
     void Update()
     {
         // Atualiza o tempo desde o último dano
         tempoDesdeUltimoDano += Time.deltaTime;
+        
+        MovimentoDaNuvens();
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -25,5 +28,10 @@ public class NuvemDePoeira : MonoBehaviour
             }
             tempoDesdeUltimoDano = 0f; // Reseta o tempo desde o último dano
         }
+    }
+
+    private void MovimentoDaNuvens()
+    {
+        transform.Translate(Vector3.left * velocidade* Time.deltaTime);
     }
 }
